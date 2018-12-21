be972e8f3e40967999f9d3ce30e477a5700a9ee6
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2018-12-21 13:40:47 +0800

    Bitmap as background. Work in progress...

---
+++ draw/src/main/java/com/divyanshu/draw/model/DrawingViewModel.kt
@@ -0,0 +1,19 @@
+package com.divyanshu.draw.model
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.util.concurrent.Executors
+
+class DrawingViewModel(val filepath:String): ViewModel() {
+    private val executor = Executors.newSingleThreadExecutor()
+    val bitmapLiveData: MutableLiveData<Bitmap> = MutableLiveData()
+
+    init {
+        executor.execute {
+            val bitmap = BitmapFactory.decodeFile(filepath)
+            bitmapLiveData.postValue(bitmap)
+        }
+    }
+}
