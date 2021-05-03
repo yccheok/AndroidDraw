db3e8f033bc24a3260f7a90af7e39302e030110f
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-05-03 12:55:37 +0800

    Use AndroidX.

--- draw/src/main/java/com/divyanshu/draw/model/DrawingViewModelFactory.kt
+++ draw/src/main/java/com/divyanshu/draw/model/DrawingViewModelFactory.kt
@@ -1,7 +1,7 @@
 package com.divyanshu.draw.model
 
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class DrawingViewModelFactory(private val filepath: String) : ViewModelProvider.NewInstanceFactory() {
 
