f83166a28d21e407852aecb934c382eaacfe5388
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-07-05 16:48:19 +0800

    INTENT_EXTRA_BITMAP -> INTENT_EXTRA_DRAWING_INFO

--- app/src/main/java/com/divyanshu/androiddraw/MainActivity.kt
+++ app/src/main/java/com/divyanshu/androiddraw/MainActivity.kt
@@ -2,25 +2,19 @@ package com.divyanshu.androiddraw
 
 import android.Manifest
 import android.app.Activity
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.WindowManager
-import android.widget.EditText
 import com.divyanshu.draw.activity.DrawingActivity
+import com.divyanshu.draw.activity.DrawingActivity.Companion.INTENT_EXTRA_DRAWING_INFO
+import com.divyanshu.draw.model.DrawingInfo
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.FileOutputStream
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -44,18 +38,37 @@ class MainActivity : AppCompatActivity() {
         }
         fab_add_draw.setOnClickListener {
             val intent = Intent(this, DrawingActivity::class.java)
+
+            intent.putExtra(INTENT_EXTRA_DRAWING_INFO, generateDrawingInfo())
+
             startActivityForResult(intent, REQUEST_CODE_DRAW)
         }
     }
 
+    private fun generateDrawingInfo(): DrawingInfo {
+        val path = File(getExternalFilesDir(null), "Android Draw")
+        path.mkdirs()
+        Log.e("path", path.toString())
+        val fileName = UUID.randomUUID().toString().toLowerCase() + ".png"
+        val file = File(path, fileName)
+
+        return DrawingInfo(
+                0,
+                fileName,
+                file.absolutePath,
+                0,
+                0,
+                0
+        )
+    }
+
     private fun getFilesPath(): ArrayList<String>{
         val resultList = ArrayList<String>()
-        val imageDir = "${Environment.DIRECTORY_PICTURES}/Android Draw/"
-        val path = Environment.getExternalStoragePublicDirectory(imageDir)
+        val path = File(getExternalFilesDir(null), "Android Draw");
         path.mkdirs()
         val imageList = path.listFiles()
-        for (imagePath in imageList){
-            resultList.add(imagePath.absolutePath)
+        imageList?.forEach {
+            resultList.add(it.absolutePath)
         }
         return resultList
     }
@@ -64,31 +77,16 @@ class MainActivity : AppCompatActivity() {
         if (data != null && resultCode == Activity.RESULT_OK) {
             when(requestCode){
                 REQUEST_CODE_DRAW -> {
-                    val result= data.getByteArrayExtra(DrawingActivity.INTENT_EXTRA_BITMAP)
-                    val bitmap = BitmapFactory.decodeByteArray(result, 0, result.size)
-                    showSaveDialog(bitmap)
+                    val imageInfo = data.getParcelableExtra<DrawingInfo>(DrawingActivity.INTENT_EXTRA_DRAWING_INFO)
+                    if (imageInfo != null) {
+                        updateRecyclerView(Uri.fromFile(File(imageInfo.path)))
+                    }
+
                 }
             }
         }
     }
-
-    private fun showSaveDialog(bitmap: Bitmap) {
-        val alertDialog = AlertDialog.Builder(this)
-        val dialogView = layoutInflater.inflate(R.layout.dialog_save, null)
-        alertDialog.setView(dialogView)
-        val fileNameEditText: EditText = dialogView.findViewById(R.id.editText_file_name)
-        val filename = UUID.randomUUID().toString()
-        fileNameEditText.setSelectAllOnFocus(true)
-        fileNameEditText.setText(filename)
-        alertDialog.setTitle("Save Drawing")
-                .setPositiveButton("ok") { _, _ -> saveImage(bitmap,fileNameEditText.text.toString()) }
-                .setNegativeButton("Cancel") { _, _ -> }
-
-        val dialog = alertDialog.create()
-        dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-        dialog.show()
-    }
-
+    
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when(requestCode){
             PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
@@ -103,21 +101,7 @@ class MainActivity : AppCompatActivity() {
             else -> {}
         }
     }
-
-    private fun saveImage(bitmap: Bitmap, fileName: String) {
-        val imageDir = "${Environment.DIRECTORY_PICTURES}/Android Draw/"
-        val path = Environment.getExternalStoragePublicDirectory(imageDir)
-        Log.e("path",path.toString())
-        val file = File(path, "$fileName.png")
-        path.mkdirs()
-        file.createNewFile()
-        val outputStream = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream)
-        outputStream.flush()
-        outputStream.close()
-        updateRecyclerView(Uri.fromFile(file))
-    }
-
+    
     private fun updateRecyclerView(uri: Uri) {
         adapter.addItem(uri)
     }
