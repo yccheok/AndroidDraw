27c12e93a84de39e2a1389e0aaf2caa5069c334e
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-07-05 16:35:11 +0800

    Remove INTENT_EXTRA_BYTE_ARRAY.

--- draw/src/main/java/com/divyanshu/draw/activity/CancelOrSaveDialogFragment.kt
+++ draw/src/main/java/com/divyanshu/draw/activity/CancelOrSaveDialogFragment.kt
@@ -7,31 +7,23 @@ import android.support.v7.app.AlertDialog
 
 class CancelOrSaveDialogFragment : DialogFragment() {
     companion object {
-        const val INTENT_EXTRA_BYTE_ARRAY = "INTENT_EXTRA_BYTE_ARRAY"
-
-        fun newInstance(byteArray: ByteArray): CancelOrSaveDialogFragment {
+        fun newInstance(): CancelOrSaveDialogFragment {
             val cancelOrSaveDialogFragment = CancelOrSaveDialogFragment()
-            val arguments = Bundle()
-            arguments.putByteArray(INTENT_EXTRA_BYTE_ARRAY, byteArray)
-            cancelOrSaveDialogFragment.arguments = arguments
             return cancelOrSaveDialogFragment
         }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val arguments = arguments
-        val byteArray = arguments!!.getByteArray(INTENT_EXTRA_BYTE_ARRAY)
-
-        val arrayAdapter = com.divyanshu.draw.activity.CancelOrSaveArrayAdapter(context)
+        val arrayAdapter = CancelOrSaveArrayAdapter(requireContext())
 
         val alertDialogBuilder = AlertDialog.Builder(requireContext())
-                .setAdapter(arrayAdapter) { dialog, which ->
+                .setAdapter(arrayAdapter) { _, which ->
                     val activity = activity
                     if (activity is CancelOrSaveDialogListener) {
                         if (which == 0) {
                             (activity as CancelOrSaveDialogListener).onCancel()
                         } else if (which == 1) {
-                            (activity as CancelOrSaveDialogListener).onSave(byteArray)
+                            (activity as CancelOrSaveDialogListener).onSave()
                         }
                     }
                 }
