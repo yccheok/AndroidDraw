1cad9d1179712501d1e666f9ad1549b26e23aaca
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2021-07-05 16:34:15 +0800

    filepath is optional

--- draw/src/main/java/com/divyanshu/draw/model/DrawingViewModelFactory.kt
+++ draw/src/main/java/com/divyanshu/draw/model/DrawingViewModelFactory.kt
@@ -3,7 +3,7 @@ package com.divyanshu.draw.model
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 
-class DrawingViewModelFactory(private val filepath: String) : ViewModelProvider.NewInstanceFactory() {
+class DrawingViewModelFactory(private val filepath: String?) : ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return DrawingViewModel(filepath) as T
